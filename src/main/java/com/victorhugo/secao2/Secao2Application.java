26486934625c06fd92b9096a5e65410f31091559
Victor Hugo <victorhugo.vhs@hotmail.com>
2021-06-15 23:40:58 -0300

    First commit, project initialized

---
+++ src/main/java/com/victorhugo/secao2/Secao2Application.java
@@ -0,0 +1,13 @@
+package com.victorhugo.secao2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorhugo/secao2/Secao2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorhugo.secao2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
